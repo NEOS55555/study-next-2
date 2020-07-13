04266d5bf6f063882ab96987b4d2be26770406b5
neos55555 <778007921@qq.com>
2020-07-13 15:14:15 +0800

    scss

--- pages/inner/one.js
+++ pages/inner/one.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './index.scss'
 
 class One extends React.Component {
   render() {
     return (
-      <div>
+      <div className="one">
         One page
       </div>
     )
